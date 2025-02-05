ae0220172b24df2e0ed0e6dcd3ab8150518b5048
huncrew <mrdalegrant@gmail.com>
2025-02-05 14:36:41 +0000

    removed next front end template code

--- templates/CDK-Next-Enterprise-Template/packages/front-end-next/src/libs/i18nNavigation.ts
+++
@@ -1,8 +0,0 @@
-import { createSharedPathnamesNavigation } from 'next-intl/navigation';
-
-import { AllLocales } from '@/utils/AppConfig';
-
-export const { usePathname, useRouter } = createSharedPathnamesNavigation({
-  locales: AllLocales,
-  localePrefix: 'as-needed',
-});
