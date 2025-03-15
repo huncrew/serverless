9a054ae54ac843de727bed5e9173233c3eccfb28
huncrew <mrdalegrant@gmail.com>
2025-03-15 08:16:44 +0000

    feat(develop): terraform added main.tf

--- templates/Terraform-Kubernetes/terraform/main.tf
+++ templates/Terraform-Kubernetes/terraform/main.tf
@@ -1,27 +1,177 @@
-# terraform/main.tf
+###################################
+# Provider + Variables
+###################################
 provider "aws" {
   region = var.region
 }
 
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "my-eks-cluster"
+}
+
+###################################
+# VPC
+###################################
+resource "aws_vpc" "this" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Public Subnet for NAT + external
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block             = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+# Private Subnet
+resource "aws_subnet" "private_1" {
+  vpc_id                  = aws_vpc.this.id
+  cidr_block             = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = false
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+}
+
+# Public Route Table + Route
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+}
+
+resource "aws_route_table_association" "public_rt_assoc_1" {
+  subnet_id      = aws_subnet.public_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# NAT Gateway
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "this" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_1.id  # NAT in public subnet
+}
+
+# Private Route Table + Route
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this.id
+  }
+}
+
+resource "aws_route_table_association" "private_rt_assoc_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private_rt.id
+}
+
+###################################
+# IAM Roles
+###################################
+# EKS Cluster Role
+resource "aws_iam_role" "eks_cluster_role" {
+  name               = "eksClusterRole"
+  assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "eks_cluster_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "EKS_Cluster_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+# EKS Node Role
+resource "aws_iam_role" "eks_node_role" {
+  name               = "eksNodeRole"
+  assume_role_policy = data.aws_iam_policy_document.eks_node_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "eks_node_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "EKS_WorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "EKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "EC2_ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_role.name
+}
+
+###################################
+# EKS Cluster + Node Group
+###################################
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids = var.private_subnets
+    subnet_ids = [
+      aws_subnet.private_1.id,
+      aws_subnet.public_1.id
+    ]
   }
-
-  # ...
 }
 
 resource "aws_eks_node_group" "this" {
   cluster_name    = aws_eks_cluster.this.name
   node_group_name = "${var.cluster_name}-nodegroup"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = var.private_subnets
-  # ...
+
+  subnet_ids = [
+    aws_subnet.private_1.id
+  ]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
 }
 
+###################################
+# Outputs
+###################################
 output "kubeconfig" {
   value = aws_eks_cluster.this.endpoint
 }
