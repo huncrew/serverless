bde672a9d5011fd62cd52531e30f7da1abb57b3f
huncrew <mrdalegrant@gmail.com>
2025-03-16 17:35:22 +0000

    feat(develop): pipeline added for AWS access via OICD

--- templates/Terraform-Kubernetes/terraform/main.tf
+++ templates/Terraform-Kubernetes/terraform/main.tf
@@ -10,19 +10,19 @@ variable "region" {
   default = "us-east-1"
 }
 
+# Name for your ECS cluster
 variable "cluster_name" {
   type    = string
-  default = "my-eks-cluster"
+  default = "my-ecs-cluster"
 }
 
 ###################################
-# VPC
+# VPC (same as before)
 ###################################
 resource "aws_vpc" "this" {
   cidr_block = "10.0.0.0/16"
 }
 
-# Public Subnet for NAT + external
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.this.id
   cidr_block             = "10.0.1.0/24"
@@ -30,7 +30,6 @@ resource "aws_subnet" "public_1" {
   map_public_ip_on_launch = true
 }
 
-# Private Subnet
 resource "aws_subnet" "private_1" {
   vpc_id                  = aws_vpc.this.id
   cidr_block             = "10.0.2.0/24"
@@ -38,12 +37,10 @@ resource "aws_subnet" "private_1" {
   map_public_ip_on_launch = false
 }
 
-# Internet Gateway
 resource "aws_internet_gateway" "this" {
   vpc_id = aws_vpc.this.id
 }
 
-# Public Route Table + Route
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.this.id
 
@@ -58,17 +55,15 @@ resource "aws_route_table_association" "public_rt_assoc_1" {
   route_table_id = aws_route_table.public_rt.id
 }
 
-# NAT Gateway
 resource "aws_eip" "nat_eip" {
   vpc = true
 }
 
 resource "aws_nat_gateway" "this" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_1.id  # NAT in public subnet
+  subnet_id     = aws_subnet.public_1.id
 }
 
-# Private Route Table + Route
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.this.id
 
@@ -84,94 +79,132 @@ resource "aws_route_table_association" "private_rt_assoc_1" {
 }
 
 ###################################
-# IAM Roles
+# ECS Cluster
 ###################################
-# EKS Cluster Role
-resource "aws_iam_role" "eks_cluster_role" {
-  name               = "eksClusterRole"
-  assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume_role_policy.json
+resource "aws_ecs_cluster" "this" {
+  name = var.cluster_name
 }
 
-data "aws_iam_policy_document" "eks_cluster_assume_role_policy" {
+###################################
+# IAM Role for Fargate Tasks
+###################################
+data "aws_iam_policy_document" "ecs_task_assume_role_policy" {
   statement {
     actions = ["sts:AssumeRole"]
     principals {
       type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
+      identifiers = ["ecs-tasks.amazonaws.com"]
     }
   }
 }
 
-resource "aws_iam_role_policy_attachment" "EKS_Cluster_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.eks_cluster_role.name
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = data.aws_iam_policy_document.ecs_task_assume_role_policy.json
 }
 
-# EKS Node Role
-resource "aws_iam_role" "eks_node_role" {
-  name               = "eksNodeRole"
-  assume_role_policy = data.aws_iam_policy_document.eks_node_assume_role_policy.json
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-data "aws_iam_policy_document" "eks_node_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
+###################################
+# Security Group for Fargate Service
+###################################
+resource "aws_security_group" "fargate_sg" {
+  name   = "fargate-service-sg"
+  vpc_id = aws_vpc.this.id
+
+  # Example: allow inbound on port 3000 from anywhere
+  # Adjust or remove if you want different rules
+  ingress {
+    description = "Allow inbound on 3000"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_iam_role_policy_attachment" "EKS_WorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.eks_node_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "EKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.eks_node_role.name
-}
-
-resource "aws_iam_role_policy_attachment" "EC2_ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.eks_node_role.name
-}
-
 ###################################
-# EKS Cluster + Node Group
+# ECS Task Definition (Fargate)
 ###################################
-resource "aws_eks_cluster" "this" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
+resource "aws_ecs_task_definition" "this" {
+  family                   = "my-fargate-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
-  vpc_config {
-    subnet_ids = [
-      aws_subnet.private_1.id,
-      aws_subnet.public_1.id
+  container_definitions = <<EOF
+[
+  {
+    "name": "my-products",
+    "image": "YOUR_ECR_REPO_URI:latest", 
+    "essential": true,
+    "portMappings": [
+      {
+        "containerPort": 3000,
+        "hostPort": 3000
+      }
     ]
   }
+]
+EOF
 }
 
-resource "aws_eks_node_group" "this" {
-  cluster_name    = aws_eks_cluster.this.name
-  node_group_name = "${var.cluster_name}-nodegroup"
-  node_role_arn   = aws_iam_role.eks_node_role.arn
+###################################
+# ECS Service (Fargate)
+###################################
+resource "aws_ecs_service" "this" {
+  name            = "my-fargate-service"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.this.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
 
-  subnet_ids = [
-    aws_subnet.private_1.id
-  ]
-
-  scaling_config {
-    desired_size = 2
-    max_size     = 3
-    min_size     = 1
+  network_configuration {
+    subnets          = [aws_subnet.private_1.id, aws_subnet.public_1.id]
+    security_groups  = [aws_security_group.fargate_sg.id]
+    assign_public_ip = true  # Set to false if you want only private
   }
+
+  # If you want to attach an ALB for HTTP traffic, you'd define a load_balancer block here.
+  # For example:
+  # load_balancer {
+  #   target_group_arn = aws_lb_target_group.this.arn
+  #   container_name   = "my-products"
+  #   container_port   = 3000
+  # }
+  #
+  # depends_on = [aws_lb_listener.http]
+
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 200
 }
 
 ###################################
 # Outputs
 ###################################
-output "kubeconfig" {
-  value = aws_eks_cluster.this.endpoint
+output "vpc_id" {
+  description = "The ID of the VPC"
+  value       = aws_vpc.this.id
+}
+
+output "ecs_cluster_name" {
+  description = "The name of the ECS cluster"
+  value       = aws_ecs_cluster.this.name
+}
+
+output "fargate_service_name" {
+  description = "The name of the Fargate service"
+  value       = aws_ecs_service.this.name
 }
