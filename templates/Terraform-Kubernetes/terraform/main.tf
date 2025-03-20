96e2288d178ced3e87d53130d47efcd30ea64bc6
huncrew <mrdalegrant@gmail.com>
2025-03-20 09:02:26 +0000

    feat(develop): adding ALB

--- templates/Terraform-Kubernetes/terraform/main.tf
+++ templates/Terraform-Kubernetes/terraform/main.tf
@@ -109,8 +109,7 @@ resource "aws_lb" "this" {
     name = "my-app-alb"
     load_balancer_type = "application"
     subnets = [
-        aws_subnet.public_1.id,
-        aws_subnet.public_2.id
+        aws_subnet.public_1.id
     ]
     security_groups = [aws_security_group.alb_sg.id]
 }
